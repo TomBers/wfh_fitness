5c12f4df993c0bd621d74d752508b59ae670f6ee
TomBers <tjcberman@gmail.com>
2022-02-14 21:25:23 +0000

    timex broke

--- lib/wfh_fitness_web/live/calendar.ex
+++ lib/wfh_fitness_web/live/calendar.ex
@@ -4,44 +4,36 @@ defmodule Calendar do
 
   def render(assigns) do
     ~H"""
-    <div class="container">
-        <table class="w-full mt-4 border border-gray-200 rounded-lg shadow-lg">
-            <thead>
-            <tr>
-                <%= for day_name <- @day_names do %>
-                <th class="text-xs p-2 text-gray-600 border border-gray-200">
-                    <%= day_name %>
-                </th>
-                <% end %>
-            </tr>
-            </thead>
-            <tbody>
-            <%= for week <- @week_rows do %>
-            <tr>
-                <%= for day <- week do %>
-                 <.live_component module={CalendarDayComponent} day={day} selected_date={@selected_date} day_class={day_class(day, @current_date, @program)} id={day.date} />
-                <% end %>
-            </tr>
+      <div class="container">
+        <div class="grid grid-cols-7">
+          <%= for day_name <- @day_names do %>
+            <div class="text-lg p-2 text-slate-400 bg-slate-800 border border-gray-200"><%= day_name %></div>
+          <% end %>
+          <%= for week <- @week_rows do %>
+            <%= for day <- week do %>
+              <div class={day_class(day, @current_date, @program)}>
+                <.live_component module={CalendarDayComponent} day={day} selected_date={@selected_date} day_class={day_class(day, @current_date, @program)} id={day.date} />
+              </div>
             <% end %>
-            </tbody>
-        </table>
-    </div>
+          <% end %>
+        </div>
+      </div>
     """
   end
 
   defp day_class(day, current_date, program) do
     cond do
       today?(day) ->
-        "text-xs p-2 text-gray-600 border border-gray-200 bg-green-200 hover:bg-green-300 cursor-pointer"
+        "text-sm p-2 text-gray-600 border border-gray-200 bg-green-300 hover:bg-green-200 cursor-pointer"
 
       missed?(day, program) ->
-        "text-xs p-2 border border-gray-200 bg-red-200 cursor-not-allowed"
+        "text-sm p-2 border border-gray-200 bg-red-200 cursor-not-allowed"
 
       other_month?(day, current_date) ->
-        "text-xs p-2 text-gray-400 border border-gray-200 bg-gray-200 cursor-not-allowed"
+        "text-sm p-2 text-gray-400 border border-gray-200 bg-gray-200 cursor-not-allowed"
 
       true ->
-        "text-xs p-2 text-gray-600 border border-gray-200 bg-white hover:bg-blue-100 cursor-pointer"
+        "text-sm p-2 text-slate-400 border border-gray-200 bg-slate-800 hover:bg-slate-600 cursor-pointer"
     end
   end
 
--- lib/wfh_fitness_web/live/calendar_day_component.ex
+++ lib/wfh_fitness_web/live/calendar_day_component.ex
@@ -4,32 +4,35 @@ defmodule CalendarDayComponent do
 
   def render(assigns) do
     ~H"""
-    <td {%{"phx-click" => "pick-date", "phx-value-date" => @day.date, "class" => @day_class}}>
+    <div {%{"phx-click" => "pick-date", "phx-value-date" => @day.date}}>
       <%= Timex.format!(@day.date, "%d", :strftime) %>
       <%= if @day.program do %>
         <p>💪</p>
         <%= if is_selected(@day.date, @selected_date) do %>
-          <button {%{"phx-click" => "skip-day", "phx-value-date" => @day.date, "class" => "btn"}}>Skip</button>
-          <table class="min-w-full shadow-md rounded">
-            <thead class="bg-gray-50">
+          <div class="align-items-end">
+            <button {%{"phx-click" => "skip-day", "phx-value-date" => @day.date, "class" => "btn btn-sm"}}>Skip</button>
+            <button {%{"phx-click" => "pick-date", "phx-value-date" => @day.date, "class" => "btn btn-sm"}}>Close</button>
+          </div>
+          <table class="table-fixed">
+            <thead>
               <tr>
-                <th class="p-4 text-left font-bold">Exercise</th>
-                <th class="p-4 text-left font-bold">Reps</th>
-                <th class="p-4 text-left font-bold">Weight</th>
+                <th class="p-2 text-left font-bold min-w-[50%]">Exercise</th>
+                <th class="p-2 text-left font-bold">Reps</th>
+                <th class="p-2 text-left font-bold">Weight</th>
               </tr>
             </thead>
-            <tbody class="divide-y divide-gray-300">
+            <tbody>
               <%= for exercise <- @day.program.exercises do %>
                 <tr>
-                  <td class="p-4"><%= exercise.name %></td>
-                  <td class="p-4"><%= exercise.reps %></td>
-                  <td class="p-4"><%= exercise.weight %></td> </tr>
+                  <td class="p-2"><%= exercise.name %></td>
+                  <td class="p-2"><%= exercise.reps %></td>
+                  <td class="p-2"><%= exercise.weight %></td> </tr>
               <% end %>
             </tbody>
           </table>
       <% end %>
       <% end %>
-    </td>
+    </div>
     """
   end
 
